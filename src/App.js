6bff484098bd50d9b8cae4453e83080ea177bb52
Pruthviraj Auti <ytvancedpruthvi@gmail.com>
2022-09-01 19:02:06 +0530

    completeing about section

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+import Home from "./components/Home/Home";
+import About from './components/About/About';
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main className='main'>
+        <Home />
+        <About />
+      </main>
+    </>
+  );
+}
+
+export default App;
